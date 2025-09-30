4531826aa942b623a95f3e390805558bb784f60c
The BlindEye <blindeyethe@gmail.com>
2025-09-30 20:23:48 +0300

    unit script

--- MyFirstARProject!/Assets/Scripts/Soldier.cs
+++ MyFirstARProject!/Assets/Scripts/Soldier.cs
@@ -1,18 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Soldier : MonoBehaviour
+namespace IRM
 {
-    // Start is called before the first frame update
-    void Start()
+    internal sealed class Unit : MonoBehaviour
     {
+        [SerializeField] private float detectionRadius = 5f;
+        [SerializeField] private LayerMask enemyLayerMask;
         
-    }
+        private readonly Collider[] _hits = new Collider[5];
+        
+        private Transform _transform;
+        private IDamageable _damageable;
+        
+        private void Awake() =>
+            _transform = transform;
+            
+        private void Update()
+        {
+            var closestEnemy = GetClosestEnemy();
+            if (closestEnemy == null)
+                return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            _damageable.DoDamage(closestEnemy.transform);
+        }
+
+        private Collider GetClosestEnemy()
+        {
+            var hitCount = Physics.OverlapSphereNonAlloc(_transform.position, detectionRadius, _hits, enemyLayerMask);
+            if (hitCount == 0)
+                return null;
+            
+            Collider closestEnemy = null;
+            float closestDistance = float.MaxValue;
+
+            for (var i = 0; i < hitCount; i++)
+            {
+                var hitPosition = _hits[i].bounds.center;
+                var directionToHit = hitPosition - _transform.position;
+
+                var distance = directionToHit.sqrMagnitude;
+                if(distance >= closestDistance) 
+                    continue;
+                
+                closestEnemy = _hits[i];
+                closestDistance = distance;
+            }
+
+            return closestEnemy;
+        }
     }
 }
+        
